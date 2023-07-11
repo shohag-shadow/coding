cecc32679d074c2fdea1a6fffcac3422d345656c
Shohag <mdshohagshowdagor001@gmail.com>
2023-07-11 08:14:18 +0600

    Added my template

---
+++ Number theory/my_template.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+const ll N=1e7+7;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ll tst;
+    cin>>tst;
+    while(tst--)
+    {
+        
+    }
+}
