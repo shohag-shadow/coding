f8c6c7b560e5b8a89865eab642b695489022eea9
Shohag <mdshohagshowdagor001@gmail.com>
2023-07-14 16:51:15 +0600

    added binary exponentiation

---
+++ Number theory/binary_exponentiation.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+ll binary_exponetiation(ll a,ll b)
+{
+    ll ans=1;
+    while(b)
+    {
+        if(b%2)
+        {
+            ans*=a;
+            b--;
+        }
+        else
+        {
+            a*=a;
+            b/=2;
+        }
+    }
+    return ans;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout<<binary_exponetiation(2,3)<<endl;
+}
